62e22526270b3458ff0d3d28f354590575e52921
zohanaiderak <zohanaiderak@users.noreply.github.com>
2020-05-14 07:33:33 -0400

    Header Complete

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Components/Header/header'
+import Header from './Components/Header/Header'
 import './App.css';
 
 function App() {
--- sprint-1/src/Components/Header/header.js
+++ sprint-1/src/Components/Header/header.js
@@ -1,2 +1,21 @@
 import React from 'react';
-import Logo from '../../'
+import Logo from '../../Assets/Logo/Logo-brainflix.svg';
+import UserImg from '../../Assets/Images/Mohan-muruge.jpg'
+import Styles from './Header.scss'
+import Search from '../../Assets/Icons/SVG/Icon-search.svg'
+
+
+function Header(){
+    return(
+        <header className = "header">
+            <span className="header__logo"><img src={Logo}></img></span>
+            <input className="header__search" type="text" placeholder="Search"></input>
+            <div className="header__user">
+                <button className="header__user--button">+ Upload</button>
+                <img className="header__user--img" src={UserImg}></img>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
