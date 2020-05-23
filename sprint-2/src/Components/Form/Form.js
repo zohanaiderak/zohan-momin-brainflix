2a54ca7a97c244e862e693e32971f2910468becd
zohanaiderak <zohanaiderak@users.noreply.github.com>
2020-05-23 13:24:33 -0400

    sprint-2 requirements complete

--- sprint-2/src/Components/Form/Form.js
+++ sprint-2/src/Components/Form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from '../../assets/images/Upload-video-preview.jpg'
 
 class Form extends React.Component{
     state = {
@@ -27,23 +27,33 @@ class Form extends React.Component{
     render() {
     return(
         <form>
-            <p className ="uploadInput">TITLE YOUR VIDEO:{" "}
-            <input className="uploadTitle"
-                type="text"
-                name="title"
-                onChange={this.updateTitle}
-                value = {this.state.title}
-                placeholder="Add a title to your video"
-                /></p>
-            <p className="uploadInput des">ADD A VIDEO DESCRIPTION
-            <textarea className="uploadDescription"
-                type="text"
-                name="description"
-                onChange={this.updateDescription}
-                value={this.state.description}
-                placeholder="Add a description of your video"></textarea></p>
-            <button className="publishButton" type="button" onClick={this.handleClick}>PUBLISH</button>
-            <button className="publishButton cancel"type="button">CANCEL</button>
+            <div className="uploadForm">
+                <span>
+                <p className="thumbnail">VIDEO THUMBNAIL</p>
+                <img className="videoUpload" src={Image} alt="Upload Video"></img>
+                </span>
+                <span className="span2">
+                <p className ="uploadInput">TITLE YOUR VIDEO
+                <input className="uploadTitle"
+                    type="text"
+                    name="title"
+                    onChange={this.updateTitle}
+                    value = {this.state.title}
+                    placeholder="Add a title to your video"
+                    /></p>
+                <p className="uploadInput des">ADD A VIDEO DESCRIPTION
+                <textarea className="uploadDescription"
+                    type="text"
+                    name="description"
+                    onChange={this.updateDescription}
+                    value={this.state.description}
+                    placeholder="Add a description of your video"></textarea></p>
+                </span>
+            </div>
+            <div className="button__container">
+                <button className="publishButton" type="button" onClick={this.handleClick}>PUBLISH</button>
+                <button className="publishButton cancel"type="button">CANCEL</button>
+            </div>
         </form>
         )
     }
--- sprint-2/src/Pages/Upload/Upload.js
+++ sprint-2/src/Pages/Upload/Upload.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from '../../assets/images/Upload-video-preview.jpg'
 import Form from '../../Components/Form/Form'
 import './Upload.scss'
 
@@ -7,8 +6,6 @@ const Upload = () =>{
     return(
         <div className="upload-page">
         <h1 className="uploadHeading">Upload Video</h1>
-        <p className="thumbnail">VIDEO THUMBNAIL</p>
-        <img className="videoUpload" src={Image} alt="Upload Video"></img>
         <Form />
         </div>
     )
